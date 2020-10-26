a113abfc6b15788885a81327ac4b495f0fa06a7f
“Shrenika123” <shrenika.nataraj@gmail.com>
2020-10-26 08:16:51 +0530

    adding logger functinality

--- server.js
+++ server.js
@@ -3,19 +3,8 @@ const app=express()
 const port=process.env.PORT || 8000
 const auth=require('./helper/Auth')
 const timer=require('./helper/timer')
-// var morgan = require('morgan')
-// app.use(morgan('dev'))
-
-
-
-// morgan((tokens, req, res) => {
-//   console.log("wow")
-//   return [
-//     tokens.method(req, res),
-//     tokens.url(req, res),
-//     tokens.status(req, res),
-//   ].join(' ')
-// })
+var morgan = require('morgan')
+app.use(morgan('dev'))
 
 app.get('/reports/pages/',auth,timer,async(req,res)=>{
   try{
